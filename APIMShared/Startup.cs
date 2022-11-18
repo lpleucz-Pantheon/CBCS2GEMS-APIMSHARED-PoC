ba6a7ca29b62a8a1a071f66e67debd284534ef49
Lukas Leucz <lukas.p.leucz@em-msph.com>
2022-11-18 19:02:44 -0300

    changes

--- APIMShared/Startup.cs
+++ APIMShared/Startup.cs
@@ -1,18 +1,9 @@
 using APIMShared.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace APIMShared
 {
